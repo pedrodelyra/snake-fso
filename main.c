4efb7f1a9d6fea711c6d698e0017411b1ab269c2
pedrodelyra <pedrodelyra@gmail.com>
2015-03-24 18:25:45 -0300

    turning screen's reference a local variable and commiting executable file

--- main.c
+++ main.c
@@ -4,24 +4,22 @@
 #include "snake.h"
 #include "io.h"
 
-static Screen *screen;
-
 // function that will be executed as a thread to read user's input
-void *read_user_data(void *arg) {
-	GAME_STATUS = PLAYING;	
+void *read_user_data(void *screen) {
+	GAME_STATUS = PLAYING;
 	char user_input;	
 	while(GAME_STATUS != GAME_OVER) {
 		user_input = set_input(read_input());
-		move_snake(screen, user_input);
+		move_snake((Screen *) screen, user_input);
 	}
-
+	pthread_exit("Exit successfully");
 }
 
 int main(void) {
 	GAME_STATUS = PLAYING;
-	screen = create_screen(30, 12);
+	Screen * const screen = create_screen(30, 12);
 	pthread_t thread_id;
-	pthread_create(&thread_id, NULL, &read_user_data, NULL);
+	pthread_create(&thread_id, NULL, read_user_data, screen);
 	while(GAME_STATUS != GAME_OVER) {
 		set_screen(screen);
 		display_screen(screen);
@@ -29,6 +27,7 @@ int main(void) {
 		wait_for(0.1);
 		system("clear");
 	}
+	//free_resources(screen);
 	printf("\n\n\n***** GAME OVER *****\n\n\n");
 	printf("SCORE: %d POINTS\n\n\n", screen->score);	
 	return 0;
